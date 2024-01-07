d7f40bec915234c4b41606811ba4e8b93aa8e8a2
KimYoungJin <kyj732422@gmail.com>
2024-01-08 02:03:19 +0900

    문제 18번

--- main.dart
+++ main.dart
@@ -1,15 +1,18 @@
 /**
-[문제 6]
-함수를 활용해보는 문제입니다. Dart 언어를 사용하여, 
-두 정수의 곱을 반환하는 함수를 작성하고,
- 그 함수를 호출하여 결과를 출력하는 프로그램을 작성해보세요.
- 예를 들어, 함수의 이름은 'multiply'로 하고,
-  7과 3을 인자로 전달하여 그 곱을 출력해보세요.
+[문제 17]
+Dart 언어를 사용하여, 함수와 Optional Parameter를 활용하는 문제입니다.\
+ 이름과 나이를 인자로 받는 'introduce'라는 함수를 작성해보세요.
+  이때, 나이는 Optional Parameter로 처리하되,
+   나이가 주어지지 않은 경우에는 "안녕하세요, 제 이름은 [name]입니다."라고 출력하고,
+    나이가 주어진 경우에는
+     "안녕하세요, 제 이름은 [name]이고, 나이는 [age]입니다."라고 출력되도록 하세요.
 */
-
-num multiply(num a, num b) => a * b;
-
 void main() {
-  num c = multiply(7, 3);
-  print(c);
+  Map<String, dynamic> kim = {
+    "name": "Kim",
+    "age": 19,
+  };
+  for (var A in kim.entries) {
+    print("${A.key}는 ${A.value}");
+  }
 }
